4ba7bdd2f6b7b162a48d5357b6fed22a5301550e
maciel252 <rafael.souza22@aluno.semai.br>
2022-11-09 21:45:15 -0300

    ProjetoNovo Prj-java

---
+++ src/main/java/org/senai/prjjava/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.senai.prjjava.repository;
+
+import org.senai.prjjava.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+    
+}
